bd529ebb8582e4584e3ee9e0fe775b565d73def9
kali-03 <mkaliszczuk03@wp.pl>
2020-05-24 13:59:38 +0200

    better fibonacci

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,38 +1,37 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
-    int tabela[sequence];
-
-    if (sequence==0)
-    return 0;
+int fibonacci_iterative(int sequence) 
+{
+    int array[sequence];
+    array[0]=0;
+    array[1]=1;
     
+    if (sequence==0)
+       return 0;
+
     if (sequence==1) 
-    return 1;
+       return 1;
 
     else 
     {
-        for (int i=2;i<=sequence;i++){
-        tabela[0]=0;
-        tabela[1]=1;
-        tabela[i]=tabela[i-2]+tabela[i-1];
+        for (int i=2;i<=sequence;i++)
+        {
+           array[i]=array[i-2]+array[i-1];
         }
     }
-    return tabela[sequence];
-    
+    return array[sequence];
+
 }
 
 int fibonacci_recursive(int sequence) {
-    
+
     if (sequence==0)
-    return 0;
+       return 0;
 
     if (sequence==1)
-    return 1;
-
-    if (sequence==2)
-    return 1;
+       return 1;
 
     else
-    return fibonacci_recursive(sequence-1)+ fibonacci_recursive(sequence-2);
-    
+       return fibonacci_recursive(sequence-1)+ fibonacci_recursive(sequence-2);
+
 }
