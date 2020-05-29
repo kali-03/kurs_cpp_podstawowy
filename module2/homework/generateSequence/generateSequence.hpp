10d873acbac0db246943c033b00ea0c67088bb93
kali-03 <mkaliszczuk03@wp.pl>
2020-05-29 18:02:02 +0200

    GenerateSequence

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -2,6 +2,14 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    
+    if (count<0){
+      return {};
+    }
+
+    std::vector<int> vec(count);
+    for (int i=0; i < count; i++){
+        vec[i]=(i+1)*step; 
+    }
+    return {vec};
 }
