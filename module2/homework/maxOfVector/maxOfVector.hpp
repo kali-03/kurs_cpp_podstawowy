433d0e223573fc27516212df7d248efb988b612b
kali-03 <mkaliszczuk03@wp.pl>
2020-05-29 18:44:24 +0200

    maxOfVector

--- module2/homework/maxOfVector/maxOfVector.hpp
+++ module2/homework/maxOfVector/maxOfVector.hpp
@@ -1,8 +1,15 @@
 #pragma once
 #include <vector>
 #include <limits>
+#include <algorithm>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int max=vec[0];
+     
+    for (const auto& el:vec){
+        if (el>vec[0])
+          max =el;
+    }
+
+    return max;
 }
