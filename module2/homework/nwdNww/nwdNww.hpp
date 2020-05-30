ce8b82a6b65fd05ed977d8ccb9972152fd79525e
kali-03 <mkaliszczuk03@wp.pl>
2020-05-30 12:36:13 +0200

    nwdNww

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,41 @@
 #pragma once
+#include <cmath>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
-}
+    if (lhs==0){
+        return rhs;
+    }
+
+    if (rhs==0){
+        return lhs;
+    }
+
+    if ((lhs<0 && rhs<0) || rhs<0){
+        return (rhs*(-1));
+    }
+
+    if (lhs<0 && rhs>0){
+        return rhs;
+    }
+    
+    int temp;
+    while(rhs!=0){
+        temp=lhs%rhs;
+        lhs=rhs;
+        rhs=temp;
+    }
+    return lhs;
+} 
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs==0 || rhs==0){
+        return 0;
+    }
+
+    if (lhs==1 || rhs==1){
+        return 1;
+    }
+    else{
+        return (lhs*rhs)/NWD(lhs,rhs);
+    }
 }
